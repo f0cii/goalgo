b7dc89f0409676f894f8bb1c4ba3bd16c1401f72
sumorf <529808348@qq.com>
2018-11-05 11:25:40 +0800

    Add GetValue ErrNotFound

--- value_api.go
+++ value_api.go
@@ -1,8 +1,21 @@
 package goalgo
 
+import (
+	"errors"
+)
+
+var (
+	// ErrNotFound is returned when an item or index is not in the database.
+	ErrNotFound = errors.New("not found")
+)
+
 // GetValue 获取一个全局变量
 func GetValue(key string) (Value, error) {
-	return GetClient().GetValue(key)
+	value, err := GetClient().GetValue(key)
+	if err != nil && err == ErrNotFound {
+		return Value{}, nil
+	}
+	return value, err
 }
 
 // SetValue 设置一个全局变量
